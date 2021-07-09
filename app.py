81d713785e6ae5ce20a7e597cbb45ac196157b56
simoniacbrush42 <simonicabrush42@gmail.com>
2021-07-09 12:25:54 -0400

    added some basic file structure

---
+++ app.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from os import environ
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Initialize app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    #users = HomeworkUser.query.all()
+    #print('# users:', len(users))
+    return render_template("index.html")
+
+@app.route('/submit_image')
+def submit_image():
+	return render_template("submit.html")
+
+@app.route('/calories')
+def calories():
+	return render_template("calories.html") #shows the calories from the image
+
+@app.route('daily_total')
+def daily_total():
+    return render_template("daily.html")
+
+@app.route('weekly_total')
+def weekly_total():
+    return render_template("weekly.html")
+    
