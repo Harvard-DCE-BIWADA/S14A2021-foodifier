3d743fe1e0c4ef0479eaaff33d1115345543d889
simoniacbrush42 <simonicabrush42@gmail.com>
2021-07-16 15:38:28 -0400

    did a tiny bit of work on image submission

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
+import os
 from dotenv import load_dotenv
 from os import environ, error
-from flask import Flask, flash, render_template, request, url_for, redirect, jsonify, session
+from flask import Flask, flash, render_template, request, url_for, redirect, jsonify, session, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 from models.models import Db, User, Post
 from forms.forms import SignupForm, LoginForm
 from passlib.hash import sha256_crypt
 import gunicorn
+import numpy as np
+from werkzeug.utils import secure_filename
+from keras.models import Sequential, load_model
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 
 # Load environment
 load_dotenv('.env')
 
+UPLOAD_FOLDER = 'uploads'
+
 # Initialize app
 app = Flask(__name__)
 app.secret_key = environ.get('SECRET_KEY')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Initialize DB
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') # this is to solve a bug in heroku
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 print(environ.get('DATABASE_URL'))
 Db.init_app(app)
+
+
+
 
 @app.route('/')
 def index():
@@ -113,9 +124,18 @@ def profile():
 def extra_info():
     return render_template("about.html") # adds additional information about the food and health concepts + about how the model works and apis + resources
 
-@app.route('/submit_image')
-def submit_image():
-	return render_template("submit.html") # area where you can submit the image for recognition 
+@app.route('/submit_image', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            output = predict(file_path)
+    #return render_template("home.html", label=output, imagesource=file_path)
+    return render_template("submit.html") # area where you can submit the image for recognition 
 
 @app.route('/calories')
 def calories():
@@ -134,4 +154,21 @@ def history():
     return render_template('index.html') # shows previous meals/calorie counts from either previous days or weeks
 
 
-    
+#ADAPTED FROM - https://github.com/mitkir/keras-flask-image-classifier/blob/master/application.py 
+
+allowed_extensions = set(["jpg", "jpeg", "png"])
+image_size = (600, 600)
+
+#model = load_model('notebooks/saved_model.pb')  
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in allowed_extensions
+
+def predict(file):
+    img = load_img(file, target_size = image_size)
+    img = img_to_array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+    probs = model.predict(img)[0]
+    return probs
+
